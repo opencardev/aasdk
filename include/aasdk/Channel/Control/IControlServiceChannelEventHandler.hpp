e8236a671e82fbf2b639a8a5c256ff3d24257aea
sjdean <sjdean@gmail.com>
2024-11-21 12:46:49 +0000

    AAP-Proto 1.6 Support and Tidy Ups
    * Restructure AASDK Proto Protobuf files for Android Auto 1.6 Support based on information from https://milek7.pl/.stuff/galdocs/readme.md
    This fleshes out enums and other methods with their full naming conventions for better readability and understanding.
    * Restructure AASDK source/header with additional renames to clarify differences between AudioService, VideoService and AVInput. Updated to MediaSinkService which is extended by AudioMediaSinkService and VideoMediaSinkService which themselves are extended by the individual service channels.
    * Added initial GenericNotification, MediaBrowser, MediaPlaybackStatus, PhoneStatus, Radio, VendorExtension and WifiProjection services.
    * Update AASDK_LOG entries for extra consistency
    * Simplify CMAKE to build and install keeping parameters to a minimum. Default to Release mode and Raspberry PI build unless otherwise specified.

--- include/aasdk/Channel/Control/IControlServiceChannelEventHandler.hpp
+++ include/aasdk/Channel/Control/IControlServiceChannelEventHandler.hpp
@@ -1,65 +1,76 @@
-/*
-*  This file is part of aasdk library project.
-*  Copyright (C) 2018 f1x.studio (Michal Szwaj)
-*
-*  aasdk is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 3 of the License, or
-*  (at your option) any later version.
-
-*  aasdk is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with aasdk. If not, see <http://www.gnu.org/licenses/>.
-*/
+// This file is part of aasdk library project.
+// Copyright (C) 2018 f1x.studio (Michal Szwaj)
+// Copyright (C) 2024 CubeOne (Simon Dean - simon.dean@cubeone.co.uk)
+//
+// aasdk is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// aasdk is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with aasdk. If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
 #include <aasdk/Error/Error.hpp>
 #include <aasdk/Common/Data.hpp>
-#include <aasdk_proto/ShutdownRequestMessage.pb.h>
-#include <aasdk_proto/ShutdownResponseMessage.pb.h>
-#include <aasdk_proto/VersionResponseStatusEnum.pb.h>
-#include <aasdk_proto/ServiceDiscoveryRequestMessage.pb.h>
-#include <aasdk_proto/AudioFocusRequestMessage.pb.h>
-#include <aasdk_proto/NavigationFocusRequestMessage.pb.h>
-#include <aasdk_proto/PingRequestMessage.pb.h>
-#include <aasdk_proto/PingResponseMessage.pb.h>
-#include <aasdk_proto/VoiceSessionRequestMessage.pb.h>
+#include <aap_protobuf/service/control/message/AudioFocusRequest.pb.h>
+#include <aap_protobuf/service//control/message/BatteryStatusNotification.pb.h>
+#include <aap_protobuf/service/control/message/ByeByeResponse.pb.h>
+#include <aap_protobuf/service/control/message/NavFocusRequestNotification.pb.h>
+#include <aap_protobuf/service/control/message/PingRequest.pb.h>
+#include <aap_protobuf/service/control/message/PingResponse.pb.h>
+#include <aap_protobuf/service/control/message/ServiceDiscoveryRequest.pb.h>
+#include <aap_protobuf/service/control/message/VoiceSessionNotification.pb.h>
 
+namespace aasdk::channel::control {
 
-
-namespace aasdk
-{
-namespace channel
-{
-namespace control
-{
-
-class IControlServiceChannelEventHandler
-{
-public:
+  class IControlServiceChannelEventHandler {
+  public:
     typedef std::shared_ptr<IControlServiceChannelEventHandler> Pointer;
 
     IControlServiceChannelEventHandler() = default;
+
     virtual ~IControlServiceChannelEventHandler() = default;
 
-    virtual void onVersionResponse(uint16_t majorCode, uint16_t minorCode, proto::enums::VersionResponseStatus::Enum status) = 0;
-    virtual void onHandshake(const common::DataConstBuffer& payload) = 0;
-    virtual void onServiceDiscoveryRequest(const proto::messages::ServiceDiscoveryRequest& request) = 0;
-    virtual void onAudioFocusRequest(const proto::messages::AudioFocusRequest& request) = 0;
-    virtual void onShutdownRequest(const proto::messages::ShutdownRequest& request) = 0;
-    virtual void onShutdownResponse(const proto::messages::ShutdownResponse& response) = 0;
-    virtual void onNavigationFocusRequest(const proto::messages::NavigationFocusRequest& request) = 0;
-    virtual void onPingRequest(const proto::messages::PingRequest& request) = 0;
-    virtual void onPingResponse(const proto::messages::PingResponse& response) = 0;
-    virtual void onChannelError(const error::Error& e) = 0;
-    virtual void onVoiceSessionRequest(const proto::messages::VoiceSessionRequest& request) = 0;
-};
+    virtual void onVersionResponse(uint16_t majorCode, uint16_t minorCode,
+                                   aap_protobuf::shared::MessageStatus status) = 0;
 
+    virtual void onHandshake(const common::DataConstBuffer &payload) = 0;
+
+    virtual void onServiceDiscoveryRequest(
+        const aap_protobuf::service::control::message::ServiceDiscoveryRequest &request) = 0;
+
+    virtual void
+    onAudioFocusRequest(const aap_protobuf::service::control::message::AudioFocusRequest &request) = 0;
+
+    virtual void onByeByeRequest(const aap_protobuf::service::control::message::ByeByeRequest &request) = 0;
+
+    virtual void
+    onByeByeResponse(const aap_protobuf::service::control::message::ByeByeResponse &response) = 0;
+
+    virtual void onBatteryStatusNotification(const aap_protobuf::service::control::message::BatteryStatusNotification &notification) = 0;
+
+    virtual void
+    onNavigationFocusRequest(
+        const aap_protobuf::service::control::message::NavFocusRequestNotification &request) = 0;
+
+    virtual void
+    onVoiceSessionRequest(const aap_protobuf::service::control::message::VoiceSessionNotification &request) = 0;
+
+    virtual void onPingRequest(const aap_protobuf::service::control::message::PingRequest &request) = 0;
+
+    virtual void onPingResponse(const aap_protobuf::service::control::message::PingResponse &response) = 0;
+
+    virtual void onChannelError(const error::Error &e) = 0;
+
+
+  };
 }
-}
-}
+
+
